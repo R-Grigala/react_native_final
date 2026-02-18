e21507c7419a8d102637b9726500eefcd3ed064d
R-Grigala <r.grigalashvili777@gmail.com>
2026-02-19 00:54:07 +0400

    change components

---
+++ components/appInput/AppInput.tsx
@@ -0,0 +1,49 @@
+import React from "react";
+import {
+    StyleSheet,
+    TextInput,
+    TextInputProps,
+    View,
+    ViewStyle,
+} from "react-native";
+
+type AppInputPropsType = {
+  placeholder: string;
+  value?: string | undefined;
+  inputStyles?: ViewStyle | undefined;
+  onChangeText: (text: string) => void;
+} & TextInputProps &
+  ViewStyle;
+
+const AppInput = ({
+  placeholder,
+  value,
+  inputStyles,
+  onChangeText,
+  ...props
+}: AppInputPropsType) => {
+  return (
+    <View style={[styles.inputContainer, inputStyles]}>
+      <TextInput
+        {...props}
+        placeholder={placeholder}
+        placeholderTextColor="#6b7280"
+        value={value}
+        onChangeText={onChangeText}
+      />
+    </View>
+  );
+};
+
+export default AppInput;
+
+const styles = StyleSheet.create({
+  inputContainer: {
+    paddingVertical: 8,
+    marginHorizontal: 12,
+    paddingHorizontal: 12,
+    borderRadius: 8,
+    borderWidth: 1,
+    marginVertical: 8,
+  },
+});
